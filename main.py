352a9fc81c2f9343ee83817a92c84ea4bae82e6e
Dinozavrrr <113333332+DinozavrrrDan@users.noreply.github.com>
2024-01-16 22:46:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+class Book:
+    def __init__(self, id_, name, pages):
+        self.id = id_
+        self.name = name
+        self.pages = pages
+
+    def __str__(self):
+        return f'Книга "{self.name}"'
+
+    def __repr__(self):
+        return f'Book(id_={self.id}, name={self.name!r}, pages={self.pages})'
+
+
+class Library:
+    def __init__(self, books=None):
+        if books is None:
+            self.books = []
+        else:
+            self.books = books
+
+    def get_next_book_id(self):
+        if not self.books:
+            return 1
+        return self.books[-1].id + 1
+
+    def get_index_by_book_id(self, book_id):
+        for i, book in enumerate(self.books):
+            if book.id == book_id:
+                return i
+        raise ValueError(f"Книга с запрашиваемым id {book_id} не существует")
